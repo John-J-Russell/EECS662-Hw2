3a56aa6ac23e5f672b3c178ffb7373d1bf9995ac
John Russell <j362r647@ku.edu>
2017-10-07 17:39:05 -0500

    Fixed typechecker for put statements, now returns unit upon successful check

--- Core.hs
+++ Core.hs
@@ -98,7 +98,7 @@ check g (CGet e) = --If e is a CTRef type "t", return "t"
 check g (CPut key to_be_stored) = --key is the Ref, to_be_stored is the expr
     do CTRef cell_type = check g key --Get the type of the reference
        storage_type = check g to_be_stored --get type of expression
-       if cell_type == storage_type then return storage_type else Nothing
+       if cell_type == storage_type then return CTOne else Nothing --return unit or nothing
 
 --------------------------------------------------------------------------------
 -- Evaluation
